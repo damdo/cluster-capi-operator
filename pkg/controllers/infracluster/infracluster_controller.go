92ad1493117ea51a44784dc39c7b5069375ef813
Rachel Ryan <raryan@redhat.com>
2024-06-05 14:42:06 +0100

    Check and manage cluster readiness

--- pkg/controllers/infracluster/infracluster_controller.go
+++ pkg/controllers/infracluster/infracluster_controller.go
@@ -21,7 +21,6 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	configv1 "github.com/openshift/api/config/v1"
-	"github.com/openshift/cluster-capi-operator/e2e/framework"
 	"github.com/openshift/cluster-capi-operator/pkg/controllers"
 	"github.com/openshift/cluster-capi-operator/pkg/operatorstatus"
 )
@@ -42,6 +41,9 @@ const (
 	notNamespaced                     = ""
 	clusterOperatorName               = "cluster-api"
 	defaultCoreProviderComponentName  = "cluster-api"
+	// This is the managedByAnnotation value that this controller sets by default when it creates an InfraCluster object.
+	// If the managedByAnnotation key is set, and it has this as the value, it means this controller is managing the InfraCluster.
+	managedByAnnotationValueClusterCAPIOperator = ""
 )
 
 type InfraClusterController struct {
@@ -69,43 +71,6 @@ func (r *InfraClusterController) Reconcile(ctx context.Context, req ctrl.Request
 	return res, nil
 }
 
-func (r *InfraClusterController) createAWSCluster(ctx context.Context) error {
-
-	apiUrl, err := url.Parse(r.Infra.Status.APIServerInternalURL)
-	if err != nil {
-		return fmt.Errorf("failed to parse apiUrl: %w", err)
-	}
-
-	port, err := strconv.ParseInt(apiUrl.Port(), 10, 32)
-	if err != nil {
-		return fmt.Errorf("failed to parse apiUrl port: %w", err)
-	}
-
-	if r.Infra.Status.PlatformStatus == nil {
-		return fmt.Errorf("infrastructure PlatformStatus should not be nil: %w", err)
-	}
-
-	awsCluster := &awsv1.AWSCluster{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      defaultInfraClusterName,
-			Namespace: framework.CAPINamespace,
-		},
-		Spec: awsv1.AWSClusterSpec{
-			Region: r.Infra.Status.PlatformStatus.AWS.Region,
-			ControlPlaneEndpoint: clusterv1.APIEndpoint{
-				Host: apiUrl.Hostname(),
-				Port: int32(port),
-			},
-		},
-	}
-
-	if err := r.Create(ctx, awsCluster); err != nil {
-		return fmt.Errorf("failed to create %q: %w", awsCluster.Kind, err)
-	}
-
-	return nil
-}
-
 // reconcile performs the main business logic for installing Cluster API components in the cluster.
 // Notably it fetches CAPI providers "transport" ConfigMap(s) matching the required labels,
 // it extracts from those ConfigMaps the embedded CAPI providers manifests for the components
@@ -130,19 +95,96 @@ func (r *InfraClusterController) reconcile(ctx context.Context) (ctrl.Result, er
 	return ctrl.Result{}, nil
 }
 
+// reconcileAWSCluster reconciles an AWSCluster object.
 func (r *InfraClusterController) reconcileAWSCluster(ctx context.Context) (ctrl.Result, error) {
 	target := awsv1.AWSCluster{ObjectMeta: metav1.ObjectMeta{
 		Name:      defaultInfraClusterName,
 		Namespace: defaultCAPINamespace,
 	}}
-	err := r.Get(ctx, client.ObjectKeyFromObject(&target), &target)
-	if err != nil && errors.IsNotFound(err) {
-		if err := r.createAWSCluster(ctx); err != nil {
-			return ctrl.Result{}, fmt.Errorf("failed to generate %q: %w", target.Kind, err)
+
+	// Checking whether AWSCluster object exists. If it doesn't, create it.
+	if err := r.Get(ctx, client.ObjectKeyFromObject(&target), &target); err != nil && errors.IsNotFound(err) {
+		apiUrl, err := url.Parse(r.Infra.Status.APIServerInternalURL)
+		if err != nil {
+			return ctrl.Result{}, fmt.Errorf("failed to parse apiUrl: %w", err)
 		}
+
+		port, err := strconv.ParseInt(apiUrl.Port(), 10, 32)
+		if err != nil {
+			return ctrl.Result{}, fmt.Errorf("failed to parse apiUrl port: %w", err)
+		}
+
+		if r.Infra.Status.PlatformStatus == nil {
+			return ctrl.Result{}, fmt.Errorf("infrastructure PlatformStatus should not be nil: %w", err)
+		}
+
+		awsCluster := &awsv1.AWSCluster{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      defaultInfraClusterName,
+				Namespace: defaultCAPINamespace,
+				// The ManagedBy Annotation is set so CAPI ignores the InfraCluster object,
+				// as that's managed externally.
+				Annotations: setManagedByAnnotation(nil),
+			},
+			Spec: awsv1.AWSClusterSpec{
+				Region: r.Infra.Status.PlatformStatus.AWS.Region,
+				ControlPlaneEndpoint: clusterv1.APIEndpoint{
+					Host: apiUrl.Hostname(),
+					Port: int32(port),
+				},
+			},
+			Status: awsv1.AWSClusterStatus{
+				// Status is set to Ready to indicate that cluster infrastructure is
+				// ready for Day 1 operations.
+				Ready: true,
+			},
+		}
+
+		if err := r.Create(ctx, awsCluster); err != nil {
+			return ctrl.Result{}, fmt.Errorf("failed to create %q: %w", awsCluster.Kind, err)
+		}
+
+		return ctrl.Result{}, nil
 	} else if err != nil {
 		return ctrl.Result{}, fmt.Errorf("failed to get %q: %w", target.Kind, err)
 	}
+
+	// At this point, the AWSCluster exists.
+	managedByAnnotationVal, found := target.Annotations[clusterv1.ManagedByAnnotation]
+	if !found {
+		// Couldn't find the managedByAnnotation on the AWSCluster object,
+		// This means by definition that the object is directly managed by CAPI.
+		// No action needs to be taken.
+		return ctrl.Result{}, nil
+	}
+
+	switch managedByAnnotationVal {
+	case managedByAnnotationValueClusterCAPIOperator:
+		// If we are here, it means it is this controllers responsibility to manage this InfraCluster object.
+		if target.Status.Ready {
+			// The Infrastructure for this CAPI Cluster is already ready - nothing to do.
+			return ctrl.Result{}, nil
+		}
+
+		targetCopy := target.DeepCopyObject().(*awsv1.AWSCluster)
+		targetPatchCopy, ok := targetCopy.DeepCopyObject().(client.Object)
+		if !ok {
+			return ctrl.Result{}, fmt.Errorf("unable to convert to client object")
+		}
+
+		targetCopy.Status.Ready = true
+
+		if err := r.Client.Patch(ctx, targetCopy, client.MergeFrom(targetPatchCopy)); err != nil {
+			return ctrl.Result{}, fmt.Errorf("unable to patch infra cluster: %v", err)
+		}
+	default:
+		// If we reach this step, it means it is not this controllers responsibility to manage this InfraCluster object, nor is it
+		// the CAPI infra providers responsbility to do so. This means this object was created outside of these two entities - thus
+		// the creating entity must manage its readiness.
+		klog.Infof("%s is annotated with an unrecognized externally managed annotation value %q"+
+			"- skipping as not managed by this controller.", target.Kind, managedByAnnotationVal)
+	}
+
 	return ctrl.Result{}, nil
 }
 
@@ -191,4 +233,15 @@ func (r *InfraClusterController) SetupWithManager(mgr ctrl.Manager) error {
 		// TODO: write a function that based the Infra returns a watch to the cloud specific InfraClusters objects.
 
 	return build.Complete(r)
+}
+
+// setManagedByAnnotation is setting the ManagedBy Annotation.
+// When this annotation is set, Cluster API will ignore the InfraCluster object and assume it is managed by an external entity.
+func setManagedByAnnotation(annotations map[string]string) map[string]string {
+	if annotations == nil {
+		annotations = map[string]string{}
+	}
+	annotations[clusterv1.ManagedByAnnotation] = managedByAnnotationValueClusterCAPIOperator
+
+	return annotations
 }
